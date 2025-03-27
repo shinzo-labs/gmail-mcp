73a64fe6960ab784195ddaeb27caeceae4910bd8
Austin <austin@shinzolabs.com>
2025-03-27 00:13:20 -0700

    Publish 1.0.2

--- src/index.ts
+++ src/index.ts
@@ -42,7 +42,7 @@ const RESPONSE_HEADERS_LIST = [
 
 const server = new McpServer({
   name: "Gmail-MCP",
-  version: "1.0.0",
+  version: "1.0.2",
   description: "An expansive MCP for the Gmail API"
 })
 
